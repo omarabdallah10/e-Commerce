3d58ba27ca82896b4426d519b20ad89818bc01a0
ahmedl3swy <ahmed.a.alesawy@gmail.com>
2024-01-26 17:18:33 +0200

    updated All Products Funtionality

--- js/database/Products.js
+++ js/database/Products.js
@@ -34,7 +34,7 @@ class Products {
             console.error('Error fetching products:', error);
         }
     }
-
+    //#Getters
     /**
      * Returns the products object.
      * @returns {Object} The products object.
@@ -42,7 +42,92 @@ class Products {
     getProducts() {
         return this.products;
     }
+    /**
+     * Returns the product object with the specified product ID.
+     * @param {string} productId - The product ID.
+     * @returns {Object|null} The product object.
+    */
+   getProductById(productId) {
+       return this.products[productId] || null;
+    }
 
+    /**
+     * Returns an array of product objects that belong to the specified category.
+     * @param {string} category - The category to filter products.
+     * @returns {Array} An array of product objects.
+     */
+    getProductsByCategory(category) {
+        const filteredProducts = Object.values(this.products).filter(product => product.category.toLowerCase() === category.toLowerCase());
+        return filteredProducts;
+    }
+    /**
+     * Returns an array of product objects that are available in the specified size.
+     * @param {string} size - The size to filter products.
+     * @returns {Array} An array of product objects.
+     */
+    getProductsBySize(size) {
+        const filteredProducts = Object.values(this.products).filter(product => product.size.toLowerCase().includes(size.toLowerCase()));
+        return filteredProducts;
+    }
+    /**
+     * Returns an array of product objects that fall within the specified price range.
+     * @param {number} minPrice - The minimum price.
+     * @param {number} maxPrice - The maximum price.
+     * @returns {Array} An array of product objects.
+     */
+    getProductsByPriceRange(minPrice, maxPrice) {
+        const filteredProducts = Object.values(this.products).filter(product => {
+            const productPrice = product.price;
+            return productPrice >= minPrice && productPrice <= maxPrice;
+        });
+        return filteredProducts;
+    }
+    /**
+     * Returns an array of product objects that match the specified search query.
+     * @param {string} query - The search query.
+     * @returns {Array} An array of product objects.
+     */
+    getProductsBySearchQuery(query) {
+        const filteredProducts = Object.values(this.products).filter(product => {
+            const productName = product.productName.toLowerCase();
+            return productName.includes(query.toLowerCase());
+        });
+        return filteredProducts;
+    }
+    /**
+     * Returns an array of the top N recently added products.
+     * @param {number} N - The number of products to retrieve.
+     * @returns {Array} An array of product objects.
+     */
+    getTopRecentlyAddedProducts(N) {
+        const sortedProducts = Object.values(this.products).sort((a, b) => {
+            const dateA = new Date(a.dateAdded);
+            const dateB = new Date(b.dateAdded);
+            return dateB - dateA;
+        });
+        return sortedProducts.slice(0, N);
+    }
+    /**
+     * Returns an array of the top N sold products.
+     * @param {number} N - The number of products to retrieve.
+     * @returns {Array} An array of product objects.
+     */
+    getTopSoldProducts(N) {
+        const sortedProducts = Object.values(this.products).sort((a, b) => b.quantitySold - a.quantitySold);
+        return sortedProducts.slice(0, N);
+    }
+    
+    /**Filter Section
+     * 
+    /**
+    * Checks if a product is available (Quantity > 0).
+    * @param {Object} product - The product object.
+    * @returns {boolean} True if available, false otherwise.
+    */
+    isAvailable(product) {
+        return product.stock > 0;
+    }
+    //#Crud Operations
     /**
      * Updates the products object with the provided object.
      * @param {Object} productsObj - The new products object.
@@ -52,16 +137,6 @@ class Products {
         localStorage.setItem('Products', JSON.stringify(productsObj));
         console.log('Products updated successfully.');
     }
-
-    /**
-     * Returns the product object with the specified product ID.
-     * @param {string} productId - The product ID.
-     * @returns {Object|null} The product object.
-     */
-    getProductById(productId) {
-        return this.products[productId] || null;
-    }
-
     /**
      * Updates the product object at the specified product ID with the provided product object.
      * @param {string} productId - The product ID.
@@ -74,13 +149,73 @@ class Products {
     }
 
     /**
-     * Adds a new product.
+     * Adds a new product with a unique product ID.
      * @param {Object} product - The new product object.
      */
     addProduct(product) {
+        // Get the maximum product ID and increase it by 1 to generate a new unique ID
+        const maxProductId = Math.max(...Object.keys(this.products), 0);
+        const newProductId = maxProductId + 1;
+
+        // Assign the new ID to the product before adding it
+        product.productId = `pid${newProductId}`;
         this.products[product.productId] = product;
         this.updateProducts(this.products);
+
         console.log('Product added successfully.');
+    }
+    /**
+     * Updates the quantity of a product, decrements available stock, and increments quantity sold.
+     * Throws an error if there is not enough stock.
+     * @param {string} productId - The product ID.
+     * @param {number} quantity - The quantity to update.
+     * @throws {Error} If there is not enough stock.
+     */
+    decreaseProductQuantity(productId, quantity) {
+        const product = this.getProductById(productId);
+
+        if (!product) {
+            throw new Error(`Product with ID ${productId} not found.`);
+        }
+
+        if (product.stock < quantity) {
+            throw new Error(`Not enough stock for product ${productId}. Available stock: ${product.stock}`);
+        }
+
+        // Update quantity sold and available stock
+        product.quantitySold += quantity;
+        product.stock -= quantity;
+
+        // Update the product in the products list
+        this.updateProduct(productId, product);
+
+        console.log(`Quantity updated for product ${productId}. New stock: ${product.stock}, Quantity sold: ${product.quantitySold}`);
+    }
+    
+    increaseProductQuantity(productId, quantity) {
+        const product = this.getProductById(productId);
+
+        if (!product) {
+            throw new Error(`Product with ID ${productId} not found.`);
+        }
+
+        // Update quantity sold and available stock
+        product.stock += quantity;
+
+        // Update the product in the products list
+        this.updateProduct(productId, product);
+
+        console.log(`Quantity updated for product ${productId}. New stock: ${product.stock}, Quantity sold: ${product.quantitySold}`);
+    }
+
+    /**
+     * Deletes the product with the specified product ID.
+     * @param {string} productId - The product ID.
+     */
+    deleteProduct(productId) {
+        delete this.products[productId];
+        this.updateProducts(this.products);
+        console.log('Product deleted successfully.');
     }
 }
 
--- js/product_test.js
+++ js/product_test.js
@@ -8,37 +8,84 @@ const products = new Products();
 // Getting the products object
 const productsObj = products.getProducts();
 console.log(productsObj);
+//Get Product by Category
+const productByCategory = products.getProductsByCategory('Shorts').filter(products.isAvailable);
+console.log(productByCategory);
 
-// Get product by ID
-const product1 = products.getProductById('pid2');
-console.log(product1);
+//Get Product by Size(M)
+const productBySize = products.getProductsBySize('M');
+console.log(productBySize);
+// get Products by Price 20->30
+const productByPrice = products.getProductsByPriceRange(20,30);
+console.log(productByPrice);
+//Get Product by SeachQuery
+const productBySearch = products.getProductsBySearchQuery('Comfortable');
+console.log(productBySearch);
 
-// -----------------------------------------------------------------------
+const currentDate = new Date();
+//const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}-${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+console.log(currentDate.toLocaleString());
+//Test for getTopRecentlyAddedProducts(4)
+const topRecentlyAddedProducts = products.getTopRecentlyAddedProducts(4);
+console.log(topRecentlyAddedProducts);
+//test for getTopSoldProducts
+const topSoldProducts = products.getTopSoldProducts(4);
+console.log(topSoldProducts);
 
-// ###### UPDATING DATA ##########
-// Update product details
-const productToUpdate = products.getProductById('pid3');
-productToUpdate.price = 35.99; // Update price to a new value
-products.updateProduct('pid3', productToUpdate);
+//test fo updateProductQuantity(productId, quantity)
+products.increaseProductQuantity('pid3', 10);
+console.log(products.getProductById('pid3'));
+//test for decreaseProductQuantity(productId, quantity)
+products.decreaseProductQuantity('pid3', 5);
+console.log(products.getProductById('pid3'));
 
-// -----------------------------------------------------------------------
 
-// ###### ADDING DATA ##########
-// Adding new product
-const newProduct = new Product(
-    'pid6', // productId
-    'Casual Sneakers', // productName
-    59.99, // price
-    'Comfortable and stylish casual sneakers for everyday wear.', // details
-    4.8, // rating
-    'M,L,XL', // size
-    15.0, // discount
-    30, // stock
-    'Shoes', // category
-    'img/6/1.png', // thumbnail
-    'img/6/1.png,img/6/2.png,img/6/3.png,img/6/4.png', // images
-    's3', // sellerId
-    '2022-09-05' // dateAdded
-);
 
-products.addProduct(newProduct);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Get product by ID
+// const product1 = products.getProductById('pid2');
+// console.log(product1);
+
+// // -----------------------------------------------------------------------
+
+// // ###### UPDATING DATA ##########
+// // Update product details
+// const productToUpdate = products.getProductById('pid3');
+// productToUpdate.price = 35.99; // Update price to a new value
+// products.updateProduct('pid3', productToUpdate);
+
+// // -----------------------------------------------------------------------
+
+// // ###### ADDING DATA ##########
+// // Adding new product
+// const newProduct = new Product(
+//     'pid6', // productId
+//     'Casual Sneakers', // productName
+//     59.99, // price
+//     'Comfortable and stylish casual sneakers for everyday wear.', // details
+//     4.8, // rating
+//     'M,L,XL', // size
+//     15.0, // discount
+//     30, // stock
+//     'Shoes', // category
+//     'img/6/1.png', // thumbnail
+//     'img/6/1.png,img/6/2.png,img/6/3.png,img/6/4.png', // images
+//     's3', // sellerId
+//     '2022-09-05' // dateAdded
+// );
+
+// products.addProduct(newProduct);
